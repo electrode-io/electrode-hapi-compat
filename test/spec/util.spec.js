e1ac8889c40c5717de7cadbe0952f9e016f80d78
Dat Vong <Dat.Vong@walmartlabs.com>
2018-10-11 10:42:05 -0700

    v1.0.0 releaes

---
+++ test/spec/util.spec.js
@@ -0,0 +1,80 @@
+"use strict";
+
+const hapi = require("hapi");
+const mockRequire = require("mock-require");
+
+describe("Util", () => {
+  let index;
+  let hapiServer;
+
+  beforeEach(() => {
+    hapiServer = new hapi.Server();
+    hapiServer.connection({ port: 80 });
+    hapiServer.route({
+      method: "GET",
+      path: "/test",
+      handler: (req, reply) => {
+        return reply("ok");
+      }
+    });
+  });
+
+  afterEach(() => {
+    hapiServer.stop();
+    delete require.cache[require.resolve("../../lib/util")];
+    mockRequire.stopAll();
+  });
+
+  it("test index", () => {
+    index = require("../..");
+    expect(index.isHapi17()).false;
+  });
+
+  it("test is hapi 17", () => {
+    mockRequire("hapi/package", { version: "17.2.2" });
+    index = require("../../lib/util");
+    expect(index.isHapi17()).true;
+  });
+
+  it("test is not hapi 17", () => {
+    index = require("../../lib/util");
+    expect(index.isHapi17()).false;
+  });
+  it("test allow tests to set flag", () => {
+    index = require("../../lib/util");
+    expect(index.isHapi17()).false;
+    index._testSetHapi17(true);
+    expect(index.isHapi17()).true;
+  });
+
+  it("test no hapi defaults hapi 16", () => {
+    mockRequire("hapi/package", null);
+    index = require("../../lib/util");
+    expect(index.isHapi17()).false;
+  });
+
+  it("test universalHapiPlugin on Hapi 16", () => {
+    index = require("../../lib/util");
+    const registers = {
+      hapi16: () => false,
+      hapi17: () => true
+    };
+    const pkg = { name: "Green" };
+    const plugin = index.universalHapiPlugin(registers, pkg);
+    expect(plugin.attributes.pkg).equal(pkg);
+    expect(plugin).equal(registers.hapi16);
+  });
+
+  it("test universalHapiPlugin on Hapi 17", () => {
+    mockRequire("hapi/package", { version: "17.0.0" });
+    index = require("../../lib/util");
+    const registers = {
+      hapi16: () => false,
+      hapi17: () => true
+    };
+    const pkg = { name: "Yellow" };
+    const plugin = index.universalHapiPlugin(registers, pkg);
+    expect(plugin.pkg).equal(pkg);
+    expect(plugin.register).equal(registers.hapi17);
+  });
+});
